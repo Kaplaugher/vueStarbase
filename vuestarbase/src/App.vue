13312c868987c138905d585f8e96bdf9eabdbaeb
Kaplaugher <kaplaugher94@gmail.com>
2017-06-15 12:30:01 -0400

    Initial commit

---
+++ vuestarbase/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h3>{{title}}</h3>
+    <div class="col-md-12">
+      <app-character :id="id" v-for="(id, index) in initial_ids"></app-character>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  import Character from './components/Character.vue';
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'Generate Your Team!',
+      initial_ids: [1, 13, 14]
+    }
+  },
+  methods: {
+    fetchCharacter() {
+      fetch('http://swapi.co/api/people/1', {
+        method: 'GET'
+      }).then(response => response.json())
+        .then(json => console.log(json))
+    }
+  },
+  components: {
+    appCharacter: Character
+  }
+}
+</script>
+
+<style>
+
+</style>
